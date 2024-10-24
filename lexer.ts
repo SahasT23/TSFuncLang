4f9371d424df36a5d26e23afd87a18ec4d098f3a
SahasT23 <sahas.st01@gmail.com>
2024-10-24 20:45:30 +0100

    added tokens for binary operators and equals sign

--- lexer.ts
+++ lexer.ts
@@ -13,14 +13,29 @@ export interface Token {
   type: TokenType;
 }
 
+function token (value = "", type: TokenType): Token {
+  return {value, type};
+}
+
 export function tokenise (srcCode: string): Token[] {
   const tokens = new Array<Token>();
-  const src = srcCode.split("") // Every single character will have a split
+  const src = srcCode.split(""); // Every single character will have a split
  
   // Building each token until the end of the file and then remove them -> need to make it more memory efficient later
   while (src.length > 0) {
-    g
+    if (src[0] == "(") {
+      tokens.push(token(src.shift(), TokenType.OpenParen));
+    }
+    else if (src[0] == ")") {
+      tokens.push(token(src.shift(), TokenType.CloseParen));
+    }
+    else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {  // All binary operators
+      tokens.push(token(src.shift(), TokenType.BinOp));
+    }
+    else if (src[0] == "=") {
+      tokens.push(token(src.shift(), TokenType.Equals));
+    }
   }
 
-  return tokens
+  return tokens;
 }
