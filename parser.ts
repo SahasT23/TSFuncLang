814ed724b6a99df11ef4904ceb97cb17d78fc73f
SahasT23 <sahas.st01@gmail.com>
2024-10-30 19:06:24 +0000

    Added methods to parse multiplier expressions and subtraction expressions, changed a part of the syntax tree but it is working now

--- parser.ts
+++ parser.ts
@@ -25,9 +25,23 @@ export default class Parser {
     return this.tokens[0] as Token;
   }
 
+  private eat () {
+    const prev = this.tokens.shift() as Token;
+    return prev;
+  }
+
+  private expect(type: TokenType, err: any) {
+    const prev = this.tokens.shift() as Token;
+    if (!prev || prev.type != type) {
+      console.error("Parser Error:\n", err, prev, " - Expecting: ", type);
+      Deno.exit(1);
+    }
+
+    return prev;
+  }
+
   public produceAST (srcCode: string): Program {
     this.tokens = tokenise(srcCode);
-    
     const program: Program = {
       kind: "Program",
       body: [],
@@ -36,32 +50,63 @@ export default class Parser {
     while (this.not_eof()) {
       program.body.push(this.parse_stmt()); // entry point for parser
     }
-
+    return program
+  }
     private parse_stmt (): Stmt {
 
       return this.parse_exp();
     }
 
     private parse_exp (): Expr {
+      return this.parse_add_exp();
+    }
 
+    private parse_add_exp(): Expr {
+      let left = this.parse_multipli_exp();
+
+      while (this.atF().value == "+" || this.atF().value == "-") {
+        const operator = this.eat().value;
+        const right = this.parse_multipli_exp();
+        left = {
+          kind: "BinExp",
+          left,
+          right,
+          operator,
+        } as BinExp;
+      }
+
+      return left;
+    }
+
+    private parse_multipli_exp(): Expr {
+      let left = this.parse_primary_exp();
+
+      while (this.atF().value == "/" || this.atF().value == "*" || this.atF().value == "%") {
+        const operator = this.eat().value;
+        const right = this.parse_primary_exp();
+        left = {
+          kind: "BinExp",
+          left,
+          right,
+          operator,
+        } as BinExp;
+      }
+
+      return left;
     }
 
     private parse_primary_exp (): Expr {
       const tk = this.atF().type;
 
       switch (tk) {
-        case TokenType.Number:
         case TokenType.Identifier:
-          return { kind: "Identifier" } as Identifier; 
-        case TokenType.Equals:
-        case TokenType.Let:
-        case TokenType.OpenParen:
-        case TokenType.CloseParen:
-        case TokenType.BinOp:
-        case TokenType.EOF:
+          return { kind: "Identifier", symbol: this.eat().value } as Identifier; 
+        case TokenType.Number:
+          return { kind: "NumericLiteral", value:parseFloat(this.eat().value) } as NumericLiteral; 
+        default:
+          console.error("unexpected token found during parsing", this.atF());
+          Deno.exit(1);
+          return {} as Stmt;
       }
     }
-
-    return program;
-  }
 }
